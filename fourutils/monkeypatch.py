afafb9669dea1a619728bbeb7258e0a4e79d4a9c
Oscar Rainford <oscar@fourbytes.me>
2018-07-18 18:46:44 +1000

    Added UUID monkeypatch for comparisons against strings directly.

---
+++ fourutils/monkeypatch.py
@@ -0,0 +1,22 @@
+def patch_uuid():
+    import uuid
+
+    global CustomUUID
+    class CustomUUID(uuid.UUID):
+        r"""Monkeypatched UUID class so we can check them directly
+        against strings for convenience, shouldn't have any weird
+        side-effects."""
+
+        def __eq__(self, obj):
+            if isinstance(obj, (str, bytes,)):
+                try:
+                    obj = uuid.UUID(obj)
+                except ValueError:
+                    pass
+            return super().__eq__(obj)
+        
+        def __hash__(self):
+            return hash(self.int)
+    CustomUUID.__name__ = uuid.UUID.__name__
+    uuid.UUID = CustomUUID
+patch_uuid()
